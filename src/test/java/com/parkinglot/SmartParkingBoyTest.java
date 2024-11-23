371538d329894b8f7979274acdcaec60125866fa
WUKE7 <kevin.wu@oocl.com>
2024-11-23 22:21:03 +0800

    test: SmartParkingBoyTest Case 2 should return second parking lot ticket when park given a car and second has more empty positions

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SmartParkingBoyTest {
     @Test
-    public void should_return_first_parking_lot_ticket_when_park_given_a_car_and_two_no_full_parking_lot(){
+    public void should_return_first_parking_lot_ticket_when_park_given_a_car_and_same_empty_positions(){
         //Given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
@@ -20,5 +20,23 @@ public class SmartParkingBoyTest {
         //Then
         assertNotNull(ticket);
         assertEquals(firstParkingLot.getId(),ticket.getParkingLotID());
+    }
+
+    @Test
+    public void should_return_second_parking_lot_ticket_when_park_given_a_car_and_second_has_more_empty_positions(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        smartParkingBoy.manage(firstParkingLot);
+        smartParkingBoy.manage(secondParkingLot);
+        //When
+        Ticket firstTicket = smartParkingBoy.park(new Car());
+        Ticket secondTicket = smartParkingBoy.park(new Car());
+        //Then
+        assertNotNull(firstTicket);
+        assertEquals(firstParkingLot.getId(),firstTicket.getParkingLotID());
+        assertNotNull(secondTicket);
+        assertEquals(secondParkingLot.getId(),secondTicket.getParkingLotID());
     }
 }
