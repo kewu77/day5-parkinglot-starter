f3c31d471a49b2b15c7846f3584a9f27013f3967
WUKE7 <kevin.wu@oocl.com>
2024-11-23 22:37:26 +0800

    test: SuperParkingBoyTest Case 2 should return second parking lot ticket when park given a car and second has higher empty positions rate

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -9,8 +9,8 @@ public class SuperParkingBoyTest {
     @Test
     public void should_return_first_parking_lot_ticket_when_park_given_a_car_and_same_empty_positions_rate(){
         //Given
-        ParkingLot firstParkingLot = new ParkingLot();
-        ParkingLot secondParkingLot = new ParkingLot();
+        ParkingLot firstParkingLot = new ParkingLot(10);
+        ParkingLot secondParkingLot = new ParkingLot(100);
         SuperParkingBoy superParkingBoy = new SuperParkingBoy();
         superParkingBoy.manage(firstParkingLot);
         superParkingBoy.manage(secondParkingLot);
@@ -20,5 +20,24 @@ public class SuperParkingBoyTest {
         //Then
         assertNotNull(ticket);
         assertEquals(firstParkingLot.getId(),ticket.getParkingLotID());
+    }
+
+    @Test
+    public void should_return_second_parking_lot_ticket_when_park_given_a_car_and_second_has_higher_empty_positions_rate(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(10);
+        ParkingLot secondParkingLot = new ParkingLot(100);
+        SuperParkingBoy superParkingBoy = new SuperParkingBoy();
+        superParkingBoy.manage(firstParkingLot);
+        superParkingBoy.manage(secondParkingLot);
+        //When
+        superParkingBoy.park(new Car());
+        Ticket secondTicket = superParkingBoy.park(new Car());
+        Ticket thirdTicket = superParkingBoy.park(new Car());
+        //Then
+        assertNotNull(secondTicket);
+        assertEquals(secondParkingLot.getId(),secondTicket.getParkingLotID());
+        assertNotNull(thirdTicket);
+        assertEquals(secondParkingLot.getId(),thirdTicket.getParkingLotID());
     }
 }
